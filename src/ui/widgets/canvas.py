fe1bb052d74afa6833e343cbcc9be1d74417c116
efgadget <esposito.fabrizio34@gmail.com>
2026-08-24 09:57:39 +0200

    0.6.7 foundation: reset canvas state safely for new projects

--- src/ui/widgets/canvas.py
+++ src/ui/widgets/canvas.py
@@ -27,288 +27,146 @@ from engine.image_engine import ImageEngine
 from ui.widgets.selection_overlay import SelectionOverlay
 
 
-
 class Canvas(QGraphicsView):
 
     def __init__(self, project):
-
         super().__init__()
-
 
         self.setViewportUpdateMode(
             QGraphicsView.ViewportUpdateMode.FullViewportUpdate
         )
 
-
         self.project = project
-
         self.viewport_state = Viewport()
 
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
 
-        self.scene = QGraphicsScene(
-            self
-        )
-
-        self.setScene(
-            self.scene
-        )
-
-
-        self.canvas_engine = CanvasEngine(
-            self.scene
-        )
-
-
-        self.image_engine = ImageEngine(
-            self.scene
-        )
-
+        self.canvas_engine = CanvasEngine(self.scene)
+        self.image_engine = ImageEngine(self.scene)
 
         self.selection_overlay = None
-
         self.current_selected_item = None
 
+        self.setRenderHint(QPainter.RenderHint.Antialiasing)
+        self.setBackgroundBrush(QBrush(QColor(70, 70, 70)))
 
-
-        self.setRenderHint(
-            QPainter.RenderHint.Antialiasing
-        )
-
-
-        self.setBackgroundBrush(
-            QBrush(
-                QColor(70,70,70)
-            )
-        )
-
-
-        self.setSceneRect(
-            -3000,
-            -3000,
-            6000,
-            6000
-        )
-
+        self.setSceneRect(-3000, -3000, 6000, 6000)
 
         self.setTransformationAnchor(
             QGraphicsView.ViewportAnchor.AnchorUnderMouse
         )
-
-
         self.setResizeAnchor(
             QGraphicsView.ViewportAnchor.AnchorUnderMouse
         )
 
-
         self._panning = False
-
         self._last_pos = QPoint()
 
+        self.canvas_engine.draw_project(project)
 
-        self.canvas_engine.draw_project(
-            project
-        )
+    def reset_for_project(self, project):
+        """Reset all canvas-owned state before drawing a new project."""
 
+        # Drop Python references before CanvasEngine clears/deletes scene items.
+        self.selection_overlay = None
+        self.current_selected_item = None
+        self.image_engine.image_item = None
 
+        self.project = project
+
+        # A new project starts from a clean viewport as well.
+        self.resetTransform()
+        self.viewport_state.reset()
+        self._panning = False
+        self.setCursor(Qt.CursorShape.ArrowCursor)
+
+        self.canvas_engine.draw_project(project)
 
     def update_selection(self):
-
         selected = self.scene.selectedItems()
 
-
         if not selected:
-
             if self.selection_overlay:
-
-                self.selection_overlay.setVisible(
-                    False
-                )
-
+                self.selection_overlay.setVisible(False)
 
             self.current_selected_item = None
-
             return
-
-
 
         item = selected[0]
 
-
         if item == self.current_selected_item:
-
             return
-
 
         self.current_selected_item = item
 
-
-        # elimina vecchio overlay
-
         if self.selection_overlay:
-
-            self.scene.removeItem(
-                self.selection_overlay
-            )
-
-
+            self.scene.removeItem(self.selection_overlay)
             self.selection_overlay = None
 
-
-
-        # crea overlay figlio dell'immagine
-
-        self.selection_overlay = SelectionOverlay(
-            item
-        )
-
-
-        self.selection_overlay.setParentItem(
-            item
-        )
-
-
-        self.selection_overlay.setVisible(
-            True
-        )
-
-
+        self.selection_overlay = SelectionOverlay(item)
+        self.selection_overlay.setParentItem(item)
+        self.selection_overlay.setVisible(True)
         self.selection_overlay.update()
 
-
-
-    def mousePressEvent(self,event):
-
+    def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.MiddleButton:
-
             self._panning = True
-
             self._last_pos = event.pos()
-
-
-            self.setCursor(
-                Qt.CursorShape.ClosedHandCursor
-            )
-
-
+            self.setCursor(Qt.CursorShape.ClosedHandCursor)
             event.accept()
-
             return
 
-
-        super().mousePressEvent(
-            event
-        )
-
-
+        super().mousePressEvent(event)
         self.update_selection()
 
-
-
-    def mouseMoveEvent(self,event):
-
+    def mouseMoveEvent(self, event):
         if self._panning:
-
-            delta = (
-                event.pos()
-                -
-                self._last_pos
-            )
-
-
+            delta = event.pos() - self._last_pos
             self._last_pos = event.pos()
 
-
             self.horizontalScrollBar().setValue(
-                self.horizontalScrollBar().value()
-                -
-                delta.x()
+                self.horizontalScrollBar().value() - delta.x()
             )
-
-
             self.verticalScrollBar().setValue(
-                self.verticalScrollBar().value()
-                -
-                delta.y()
+                self.verticalScrollBar().value() - delta.y()
             )
 
-
             event.accept()
-
             return
 
+        super().mouseMoveEvent(event)
 
-        super().mouseMoveEvent(
-            event
-        )
-
-
-
-    def mouseReleaseEvent(self,event):
-
+    def mouseReleaseEvent(self, event):
         if event.button() == Qt.MouseButton.MiddleButton:
-
             self._panning = False
-
-
-            self.setCursor(
-                Qt.CursorShape.ArrowCursor
-            )
-
-
+            self.setCursor(Qt.CursorShape.ArrowCursor)
             event.accept()
-
             return
 
+        super().mouseReleaseEvent(event)
 
-        super().mouseReleaseEvent(
-            event
-        )
-
-
-
-    def wheelEvent(self,event):
-
+    def wheelEvent(self, event):
         old_zoom = self.viewport_state.zoom
 
-
         if event.angleDelta().y() > 0:
-
             self.viewport_state.zoom_in()
-
         else:
-
             self.viewport_state.zoom_out()
 
-
-        factor = (
-            self.viewport_state.zoom /
-            old_zoom
-        )
-
-
-        self.scale(
-            factor,
-            factor
-        )
-
-
+        factor = self.viewport_state.zoom / old_zoom
+        self.scale(factor, factor)
 
     def fit_material(self):
-
         self.resetTransform()
-
         self.viewport_state.reset()
-
-
         self.fitInView(
             self.scene.itemsBoundingRect(),
             Qt.AspectRatioMode.KeepAspectRatio
         )
 
-
-
-    def import_image(self,filename):
-
-        return self.image_engine.load_image(
-            filename
-        )
+    def import_image(self, filename):
+        # Importing a replacement image also invalidates the old selection UI.
+        self.selection_overlay = None
+        self.current_selected_item = None
+        return self.image_engine.load_image(filename)
